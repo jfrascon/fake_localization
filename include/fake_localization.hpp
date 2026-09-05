a7eb0318c761c09931dd09d8211a9612d046eab7
jfrascon <jfrascon@gmail.com>
2026-09-05 17:13:18 +0000

    refactor: remove legacy fake_localization include

--- include/fake_localization.hpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2026 Juan Francisco Rascon Crespo
-
-#pragma once
-
-// Keep the original include path available for existing consumers.
-#include <fake_localization/fake_localization.hpp>
